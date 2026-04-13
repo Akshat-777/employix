53cff46f738adce35e999b42528a14aef041016d
Akshat <akshat77sk@gmail.com>
2026-04-13 11:44:24 +0530

    fix spacy model download URL

--- backend/python_logic/build.sh
+++ backend/python_logic/build.sh
@@ -6,6 +6,6 @@ echo "--- Installing dependencies ---"
 pip install -r requirements.txt
 
 echo "--- Downloading SpaCy model ---"
-python -m spacy download en_core_web_sm
+pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.0/en_core_web_sm-3.7.0-py3-none-any.whl
 
 echo "--- Build complete ---"
